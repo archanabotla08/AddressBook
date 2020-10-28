2f1a00a7bf3b6bc19d64b69ce85e0e670a277f3c
Nisha271 <bukkenisha@gmail.com>
2020-10-28 17:59:11 +0530

    [Archana] Added: welcome message to address book

---
+++ src/com/blz/addressbook/AddressBook.java
@@ -0,0 +1,10 @@
+package com.blz.addressbook;
+
+public class AddressBook {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome Message in master");
+	}
+
+}
