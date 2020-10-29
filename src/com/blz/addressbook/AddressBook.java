c54946d15c942b5e9cd1bce734023035010efc35
Nisha271 <bukkenisha@gmail.com>
2020-10-29 15:31:57 +0530

    [Archana] Added: UC3 to edit an existing person

--- src/com/blz/addressbook/AddressBook.java
+++ src/com/blz/addressbook/AddressBook.java
@@ -9,75 +9,15 @@ import java.util.Scanner;
 
 public class AddressBook {
 	
-	public String firstName;
-	public String lastName;
-	public String address;
-	public String city;
-	public String zip;
-	public String phoneNumber;
-	public String email;
-	
-	public static List<AddressBook> addressBookList = new ArrayList<>();
-	public AddressBook() {
-		
-	}
-	public AddressBook(String firstName,String lastName,String address,String city,String zip,String phoneNumber,String email) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.address = address;
-		this.city = city;
-		this.zip = zip;
-		this.phoneNumber = phoneNumber;
-		this.email = email;
-	}
-	
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setAddressName(String address) {
-		this.address = address;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
-	public String getZip() {
-		return zip;
-	}
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-	public String getPhoneNumber() {
-		return phoneNumber;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getEmail() {
-		return email;
-	}
-	
+	public static List<PersonDetails> addressBookList = new ArrayList<>();
+	Scanner sc = new Scanner(System.in);
 	public  void display() {
-		Iterator itr=addressBookList.iterator();//getting the Iterator  
-		while(itr.hasNext()){//check if iterator has the elements  
-			  AddressBook object_1=(AddressBook)itr.next();   
+		Iterator itr=addressBookList.iterator();  
+		if(addressBookList.isEmpty()) {
+			System.out.println("No Enteries");
+		}else {
+		while(itr.hasNext()){
+		PersonDetails object_1=(PersonDetails)itr.next();   
 		System.out.println("\n"+ "First Name: " + object_1.firstName + "\n" +
 							"Last Name: " + object_1.lastName + "\n" +
 							"Address: " + object_1.address + "\n" +
@@ -86,35 +26,99 @@ public class AddressBook {
 							"PhoneNumber: " + object_1.phoneNumber + "\n" +
 							"Email: " + object_1.email );
 		  } 
+		}
 	}
 	public void addPerson() {
 		System.out.println("Enter person details : " + "\n");
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter first Name : ");
-		setFirstName(sc.next());
+		String firstName = sc.next();
 		System.out.print("Enter last Name : ");
-		setLastName(sc.next());
+		String lastName = sc.next(); 
 		System.out.print("Enter Address : ");
-		setAddressName(sc.next());
+		String address = sc.next();
 		System.out.print("Enter City : ");
-		setCity(sc.next());
+		String city = sc.next();
 		System.out.print("Enter Zip : ");
-		setZip(sc.next());
+		String zip = sc.next();
 		System.out.print("Enter PhoneNumber : ");
-		setPhoneNumber(sc.next());
+		String phoneNumber = sc.next();
 		System.out.print("Enter Email Id : ");
-		setEmail(sc.next());
+		String email = sc.next();
 		
-		AddressBook addPerson = new AddressBook(firstName, lastName, address, city,zip,phoneNumber,email);
+		PersonDetails addPerson = new PersonDetails(firstName, lastName, address, city,zip,phoneNumber,email);
 		addressBookList.add(addPerson);
 		System.out.print("Person is Added Successfully");
 		display();
 	}
+	public void editPerson() {
+		if(addressBookList.isEmpty()) {
+			System.out.println("No Enteries in Address Book");
+		}else {
+
+			
+			for(PersonDetails i : addressBookList) {
+				System.out.println(addressBookList.indexOf(i)+ " : "+ i.getFirstName());
+			}
+			
+			System.out.println("Enter the firstName  and index to edit that person details : " );
+			String editPerson = sc.next();
+			int index = sc.nextInt();
+			while(true) {
+			System.out.println("Enter Choice to edit the details : " + "1: lastName " + "2: address " + "3: city "+"4: zip "+
+								"5: phoneNumber "+"6: email " +"7: exit");
+			int choice = sc.nextInt();
+			switch(choice) {
+			case 1:
+				System.out.println("Enter LastName: ");
+				String lastName = sc.next();
+				addressBookList.get(index).setLastName(lastName);
+				display();
+				break;
+			case 2:
+				System.out.println("Enter Address: ");
+				String address = sc.next();
+				addressBookList.get(index).setAddress(address);
+				display();
+				break;
+			case 3:
+				System.out.println("Enter City: ");
+				String city = sc.next();
+				addressBookList.get(index).setCity(city);
+				break;
+			case 4:
+				System.out.println("Enter Zip: ");
+				String zip = sc.next();
+				addressBookList.get(index).setZip(zip);;
+				break;
+			case 5:
+				System.out.println("Enter PhoneNumber: ");
+				String phoneNumber = sc.next();
+				addressBookList.get(index).setPhoneNumber(phoneNumber);
+				break;
+			case 6:
+				System.out.println("Enter Email: ");
+				String email = sc.next();
+				addressBookList.get(index).setEmail(email);
+				break;
+			case 7:
+				 addressBookCRUDOperationChoice();
+			default:
+				System.out.println("InValid Choice !!!!");
+				editPerson();
+			}
+			
+		}
+			
+		}
+	}
 	
 	public void addressBookCRUDOperationChoice() {
-		Scanner sc = new Scanner(System.in);
+		
 		int choice;
-		System.out.println("Menu Item: " + "\n" + "1: Add Person" + "\n" +"2: Display " +"\n"+"3: Exit");
+		System.out.println("Menu Item: " + "\n" + "1: Add Person" + "\n" +"2: Display " +"\n"+"3: Edit person"+"\n"+"4: Exit");
+		 Label:
+		while(true) {
 		System.out.println("Enter the choice");
 		choice = sc.nextInt();
 		switch(choice) {
@@ -124,6 +128,8 @@ public class AddressBook {
 		case 2:
 			display();
 			break;
+		case 3:
+			editPerson();
 		case 4:
 			System.exit(0);
 			break;
@@ -131,11 +137,13 @@ public class AddressBook {
 			System.out.println("Invalid Choice");
 			break;
 		}
+		
+		}
 	}
 	public static void main(String[] args) {
 		
-		AddressBook object_1 = new AddressBook("Sweety", "Botla", "Somesh Colony Nanded", "Nanded", "431552", "9898787621", "abc@gmail.com");
-		addressBookList.add(object_1);
+		AddressBook object_1 = new AddressBook();
+	//	addressBookList.add(object_1);
 		object_1.addressBookCRUDOperationChoice();
 				
 	}
---
+++ src/com/blz/addressbook/PersonDetails.java
@@ -0,0 +1,68 @@
+package com.blz.addressbook;
+
+public class PersonDetails {
+	
+	public String firstName;
+	public String lastName;
+	public String address;
+	public String city;
+	public String zip;
+	public String phoneNumber;
+	public String email;
+	
+	public PersonDetails() {
+		
+	}
+	public PersonDetails(String firstName,String lastName,String address,String city,String zip,String phoneNumber,String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getEmail() {
+		return email;
+	}
+}
